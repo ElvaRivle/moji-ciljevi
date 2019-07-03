bb7e49ec32b8453fbf103a013d8c04251d211657
ElvaRivle <elvirvlahovljak@gmail.com>
2019-07-03 08:37:36 +0200

    updated base href

--- public/Home/index_action/index.php
+++ public/Home/index_action/index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <base href="/zig/public/Home/index_action/" />
+    <base href="/moji-ciljevi/public/Home/index_action/" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link type="text/css" rel="stylesheet" href="css/main.css">
