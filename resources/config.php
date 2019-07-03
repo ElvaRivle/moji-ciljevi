3300ab79c38ac1b30e73ec6b8b0b89c48aaba6f1
ElvaRivle <elvirvlahovljak@gmail.com>
2019-07-03 07:33:43 +0200

    added DB config

--- resources/config.php
+++ resources/config.php
@@ -8,5 +8,8 @@ require_once('helper_funcs.php');
 define('DEFAULT_CONTROLLER', 'Home');
 define('DEFAULT_ACTION', 'index_action');
 
-
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'elvaroot');
+define('DB_NAME', 'moji-ciljevi');
 ?>
